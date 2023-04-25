c0b8df6e14a574bf00e769815a5db6ab84065e7a
KIDROZD <drozd.kinga10@gmail.com>
2023-04-25 20:08:24 +0200

    Tworzenie logów przez użytkownika

--- skrypt.sh
+++ skrypt.sh
@@ -7,11 +7,22 @@ while [ $# -gt 0 ]; do
             exit 0
             ;;
         --logs)
-            for i in {1..100}; do
-                echo "Nazwa pliku: log$i.txt" > log$i.txt
-                echo "Komenda: $0" >> log$i.txt
-                echo "Data stworzenia pliku: $(date)" >> log$i.txt
-            done
+            shift
+            if [[ $1 =~ ^-?[0-9]+$ ]]; then
+                n=${1#-}
+                for i in $(seq 1 $n); do
+                    echo "Nazwa pliku: log$i.txt" > log$i.txt
+                    echo "Komenda: $0" >> log$i.txt
+                    echo "Data stworzenia pliku: $(date)" >> log$i.txt
+                done
+            else
+                for i in {1..100}; do
+                    echo "Nazwa pliku: log$i.txt" > log$i.txt
+                    echo "Komenda: $0 $1" >> log$i.txt
+                    echo "Data stworzenia pliku: $(date)" >> log$i.txt
+                done
+                exit 1
+            fi
             exit 0
             ;;
     esac
