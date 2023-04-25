d687bfda66b01d5d6b61f5eb8112962db32b4897
KIDROZD <drozd.kinga10@gmail.com>
2023-04-25 20:00:26 +0200

    Tworzenie logów automatycznie

--- skrypt.sh
+++ skrypt.sh
@@ -6,6 +6,14 @@ while [ $# -gt 0 ]; do
             date
             exit 0
             ;;
+        --logs)
+            for i in {1..100}; do
+                echo "Nazwa pliku: log$i.txt" > log$i.txt
+                echo "Komenda: $0" >> log$i.txt
+                echo "Data stworzenia pliku: $(date)" >> log$i.txt
+            done
+            exit 0
+            ;;
     esac
     shift
 done
